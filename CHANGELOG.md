d2e701bc3db1547561f3b4c7525edb454dc93c33
Djamaleddine Belilet <fd_belilet@esi.dz>
2020-05-08 21:48:45 +0100

    refactor: ...

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* Wraps the content with expanded if the direction is set to vertical
+
 ## 0.0.1
 
 * Initial release
--- lib/steps.dart
+++ lib/steps.dart
@@ -20,57 +20,63 @@ class Steps extends StatelessWidget {
       scrollDirection: direction,
       itemCount: steps.length,
       itemBuilder: (context, position) => Stack(
-            children: <Widget>[
-              Positioned(
-                top: direction == Axis.horizontal
-                    ? 16 + size * 2.14 / 2 - 5 / 2
-                    : 0,
-                bottom: direction == Axis.horizontal ? null : 0,
-                left: direction == Axis.horizontal
-                    ? 0
-                    : 16 + size * 2.14 / 2 - 5 / 2,
-                right: direction == Axis.horizontal ? 0 : null,
-                height: direction == Axis.horizontal ? path['width'] : null,
-                width: direction == Axis.horizontal ? null : path['width'],
-                child: Container(
-                  color: path['color'],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(right: 16),
-                      child: Text(
-                        steps[position]['label'],
-                        style: TextStyle(
-                          color: steps[position]['color'],
-                          fontSize: size,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(6),
-                      width: size * 2.14,
-                      height: size * 2.14,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: steps[position]['background'],
-                        borderRadius: BorderRadius.circular(size * 2.14),
-                      ),
+        children: <Widget>[
+          Positioned(
+            top:
+                direction == Axis.horizontal ? 16 + size * 2.14 / 2 - 5 / 2 : 0,
+            bottom: direction == Axis.horizontal ? null : 0,
+            left:
+                direction == Axis.horizontal ? 0 : 16 + size * 2.14 / 2 - 5 / 2,
+            right: direction == Axis.horizontal ? 0 : null,
+            height: direction == Axis.horizontal ? path['width'] : null,
+            width: direction == Axis.horizontal ? null : path['width'],
+            child: Container(
+              color: path['color'],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(right: 16),
+                  child: Text(
+                    steps[position]['label'],
+                    style: TextStyle(
+                      color: steps[position]['color'],
+                      fontSize: size,
+                      fontWeight: FontWeight.w900,
                     ),
-                    steps[position]['content'] != null
+                  ),
+                  padding: EdgeInsets.all(6),
+                  width: size * 2.14,
+                  height: size * 2.14,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: steps[position]['background'],
+                    borderRadius: BorderRadius.circular(size * 2.14),
+                  ),
+                ),
+                steps[position]['content'] != null
+                    ? direction == Axis.horizontal
                         ? Padding(
                             padding: EdgeInsets.only(top: size * 2.14),
                             child: steps[position]['content'],
                           )
-                        : Container()
-                  ],
-                ),
-              )
-            ],
-          ),
+                        : Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.only(top: size * 2.14),
+                              child: steps[position]['content'],
+                            ),
+                          )
+                    : Container(),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
